ce7cc084c11467783a7791a39d294860cd36c4d1
Minh Vu <vuquangminh0507@gmail.com>
2021-12-14 16:00:41 +0700

    test change name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Minh Meo
+          Minh Fix Bug
         </p>
       </header>
     </div>
